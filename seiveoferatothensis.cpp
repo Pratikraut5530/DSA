feebeebcb97c7563c73bf9bb25bf992a37d620a6
PratikRaut5530 <pratikraut5630@gmail.com>
2023-08-09 23:25:57 +0530

    seive of eratosthensis

---
+++ seiveoferatothensis.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void seiveprime(int n,int count){
+
+    int prime[100]={0};
+    
+    for(int i=2;i<=n;i++){
+        if(prime[i]==0){
+            for(int j=i*i;j<=n;j+=i){
+                prime[j]=1;
+            }
+        }
+    }
+
+    //counting all the prime numbers
+    for(int i=2;i<=n;i++){
+        if(prime[i]==0){
+            cout<<i<<endl;
+            count++;
+        }
+    }
+
+    cout<<count<<endl;
+}
+
+int main()
+{
+    int count=0;
+    int n=9;
+    seiveprime(n,count);
+    return 0;
+}
