125c18f9238261479a2fc1aea77dbaa836889786
PratikRaut5530 <pratikraut5630@gmail.com>
2023-08-18 22:41:45 +0530

    merge sort main

--- mergeSortRecursion.cpp
+++ mergeSortRecursion.cpp
@@ -10,35 +10,40 @@ void merge(int arr[],int s,int e){
     int *second = new int[len2];
 
     //copy values
-    int mainArrayIndex = s;
+    
     for(int i=0;i<len1;i++){
-        first[i]=arr[mainArrayIndex++];
+        first[i]=arr[s+i];
     }
-
-    int k=mid+1;
     for(int i=0;i<len2;i++){
-        second[i]=arr[mainArrayIndex+1];
+        second[i]=arr[mid+1+i]; 
     }
 
     int index1 = 0;
     int index2=0;
-    mainArrayIndex = s;
+    int mainArrayIndex = s;
 
     while(index1<len1 && index2<len2){
-        if(first[index1] < second[index2]){
-            arr[mainArrayIndex++] == first[index2++];
+        if(first[index1] <= second[index2]){
+            arr[mainArrayIndex] = first[index1];
+            index1++;
         }
         else{
-            arr[mainArrayIndex++] == second[index2++];
+            arr[mainArrayIndex] = second[index2];
+            index2++;
         }
+        mainArrayIndex++;
     }
 
     while(index1 < len1){
-        arr[mainArrayIndex++]==first[index1++];
+        arr[mainArrayIndex]=first[index1];
+        mainArrayIndex++;
+        index1++;
     }
 
-    while(index2 <len2){
-        arr[mainArrayIndex++]==second[index2++];
+    while(index2 < len2){
+        arr[mainArrayIndex]=second[index2];
+        mainArrayIndex++;
+        index2++;
     }
 
 
@@ -46,7 +51,7 @@ void merge(int arr[],int s,int e){
 }
 
 void mergeSort(int arr[],int s,int e){
-    if(s>e){
+    if(s>=e){
         return ;
     
     }
@@ -55,17 +60,26 @@ void mergeSort(int arr[],int s,int e){
     mergeSort(arr,s,mid);
 
     //right part sort karna hai
-    mergeSort(arr,mid,e);
+    mergeSort(arr,mid+1,e);
 
     merge(arr,s,e);
 }
 
 int main()
 {
-    int arr[5]={2,5,1,6,9};
-    int n=5;
+    int arr[6]={3,2,1,6,9,11};
+    int n=6;
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" "; 
+    }
+    cout<<endl;
 
     mergeSort(arr,0,n-1);
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" "; 
+    }
     
     return 0;
 }
