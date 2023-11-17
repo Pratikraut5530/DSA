efd1b522028ab27367562e3e878a152d9bd3562a
PratikRaut5530 <pratikraut5630@gmail.com>
2023-11-17 19:29:25 +0530

    daily dsa

---
+++ 01knapsackdynamic.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int f(int ind,int W,vector<int> &wt,vector<int> &val,vector<vector<int>> &dp){
+    //base case
+    if(ind == 0){
+        if(wt[0]<=W){
+            return val[0];
+        }
+        return 0;
+    }
+
+    if(dp[ind][W] != -1){
+        return dp[ind][W];
+    }
+    int notTake = 0 + f(ind-1,W,wt,val,dp);
+    int take = INT8_MIN;
+    if(wt[ind]<=W){
+        take = val[ind] + f(ind-1,W-wt[ind],wt,val,dp);
+    }
+    return dp[ind][W] = max(take,notTake);
+}
+
+int knapsack(vector<int> weight,vector<int> value,int n,int maxWeight){
+    //memoisation
+    vector<vector<int>> dp(n,vector<int> (maxWeight+1,-1));
+    return f(n-1,maxWeight,weight,value,dp);
+}
+int main()
+{
+    return 0;
+}
